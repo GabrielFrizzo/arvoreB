cab4ba174047f2ab26b5b619d9122de3c47271b3
Gabriel Frizzo <gabrielfrizzo@gmail.com>
2018-10-06 05:53:40 -0300

    Fix issues with Cormen index

--- src/insereArvoreb.c
+++ src/insereArvoreb.c
@@ -6,23 +6,23 @@ Arvore* dividir_no (Arvore *x, int i, Arvore *y) {
    z->folha = y->folha;
    z->n = T-1;
    int j;
-   for (j = 1; j <= T-1; j++) {
+   for (j = 0; j <= T-2; j++) {
        z->chaves[j] = y->chaves[j+T];
    }
    if (!y->folha) {
-       for (j = 1; j <= T; j++) {
+       for (j = 0; j <= T-1; j++) {
            z->filhos[j] = y->filhos[j+T];
        }
    }
    y->n = T-1;
-   for (j = x->n + 1; j >= i+1; j--) {
+   for (j = x->n; j >= i+1; j--) {
        x->filhos[j+1] = x->filhos[j];
    }
    x->filhos[i+1] = z;
    for (j = x->n; j >= i; j--) {
        x->chaves[j+1] = x->chaves[j];
    }
-   x->chaves[i] = y->chaves[T];
+   x->chaves[i] = y->chaves[T-1];
    x->n++;
    return x;
 }
@@ -31,17 +31,16 @@ Arvore* dividir_no (Arvore *x, int i, Arvore *y) {
 Arvore* inserir_arvore_nao_cheia (Arvore *x, TIPO k) {
    int i = x->n;
    if (x->folha) {
-       while (i >= 1 && k < x->chaves[i]) {
-           x->chaves[i+1] = x->chaves[i];
+       while (i >= 1 && k < x->chaves[i-1]) {
+           x->chaves[i] = x->chaves[i-1];
            i--;
        }
-       x->chaves[i+1] = k;
+       x->chaves[i] = k;
        x->n++;
    } else {
-       while (i >= 1 && k < x->chaves[i]) {
+       while (i >= 1 && k < x->chaves[i-1]) {
            i--;
        }
-       i++;
        if (x->filhos[i]->n == 2*T-1) {
            x = dividir_no(x, i, x->filhos[i]);
            if (k > x->chaves[i])
